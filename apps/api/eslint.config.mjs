bb2f541b28e6549a13513f40f2b6c79076933268
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-01-03 10:53:33 -0300

    chore(api): test permissions

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,3 @@
+import { config } from '@saas/eslint-config/node'
+
+export default config
---
+++ apps/api/src/server.ts
@@ -0,0 +1,7 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.table({userCanDeleteOtherUsers, userCanInviteSomeoneElse, userCannotDeleteOtherUsers})
